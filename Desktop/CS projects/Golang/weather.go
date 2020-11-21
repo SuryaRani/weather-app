dd5dc8dd6d0089fc05864d043b047fdda99f7b57
Surya Rani <srani@ucsc.edu>
2020-11-21 02:30:01 -0800

    able to get user input for coordinates now gotta look up the weather

--- Desktop/CS projects/Golang/weather.go
+++ Desktop/CS projects/Golang/weather.go
@@ -4,17 +4,24 @@ import (
 	"fmt"
 	"log"
 	"time"
-	"html/template"
+	//"html/template"
 	"net/http"
 	
 )
+
+ type coordinates struct{
+	lat int
+	long int
+}
 
 
 func main(){
 	fileServer := http.FileServer(http.Dir("./static")) 
 	http.Handle("/", fileServer)
-	
-	http.HandleFunc("/",HomePage)
+	http.Handle("/form.html", fileServer)
+
+	//http.HandleFunc("/",HomePage)
+	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/weather", WeatherPage)
 	log.Fatal(http.ListenAndServe(":8000",nil))
 
@@ -36,6 +43,21 @@ func WeatherPage(w http.ResponseWriter, r *http.Request){
 
 	fmt.Println("Weather page accessed")
 
+
+
 	fmt.Fprintf(w,"Where would you like to know the weather of?\n")
 
+}
+func formHandler(w http.ResponseWriter, r *http.Request) {
+    if err := r.ParseForm(); err != nil {
+        fmt.Fprintf(w, "ParseForm() err: %v", err)
+        return
+    }
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("lat")
+    address := r.FormValue("long")
+
+    fmt.Fprintf(w, "Name = %s\n", name)
+    fmt.Fprintf(w, "Address = %s\n", address)
+    
 }
